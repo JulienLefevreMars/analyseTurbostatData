99ef0bdc57e5a7a2d77f3b5478646f03b8265a06
JulienLefevreMars <julien.lefevre@univ-amu.fr>
2020-06-04 10:23:25 +0200

    Add test files

--- script_analyse.py
+++ script_analyse.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
-    #Data
-    data=pd.read_csv("FirstTest.out",delimiter='\t')
+    # You need to run first turbostat, e.g. with
+    # sudo turbostat --interval 10 --quiet -out ThirdTest.out
+
+    # Then read the Data
+    #data=pd.read_csv("FirstTest.out",delimiter='\t')
+    data = pd.read_csv("ThirdTest.out", delimiter='\t')
 
     number_of_proc=9
     index_of_proc=0
+    time_per_step=10
     number_of_steps=int((data.shape[0]+1)/(number_of_proc+1))-1
     columns=[46,47,48,49]
 
-    time=np.linspace(0,20*(number_of_steps),number_of_steps+1)
-    #for index_of_proc in range(number_of_proc):
+    time=np.linspace(0,time_per_step*(number_of_steps),number_of_steps+1)
     for column in columns:
         plt.plot(time/60,np.array(data.iloc[index_of_proc+(number_of_proc+1)*np.array([i for i in range(number_of_steps+1)]),column],dtype=float))
 
